eef3d238c7c910247cf141cb3e0dbf5cbbdb5e5f
mareped <92785079+mareped@users.noreply.github.com>
2023-04-20 00:46:28 +0200

    Multiclass now works with roc_cruves. Made the evaluator object oriented.

--- evaluation/clf_framework.py
+++ evaluation/clf_framework.py
@@ -2,125 +2,122 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+from utilities.plots import plot_multiclass_roc, plot_binaryclass_roc
+from config import DataConfig
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, roc_auc_score, auc, f1_score, roc_curve
-from utilities.plots import plot_multiclass_roc, plot_roc_binaryclass
-
-# define dataset name you are using (lower_back_pain, obesity)
+from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.preprocessing import label_binarize
 
-DATASET_NAME = 'lower_back_pain'
 
-# define dataset model you are using (ctgan, copulagan)
-MODEL_NAME = 'ctgan'
+class ClassifierEvaluator:
+    def __init__(self):
+        self.classes = None
 
-# Hyperparameters
-EPOCHS = 800
-BATCH_SIZE = 100
+    def read_data(self, dataset_path):
+        dataset = pd.read_csv(dataset_path)
 
-file_ending = f'{MODEL_NAME}_{EPOCHS}_epochs_{BATCH_SIZE}_batch'
+        x = dataset.iloc[:, :-1].values
+        y = dataset.iloc[:, -1].values
 
-# Define where the real and fake data path is. IMPORTANT: change real file name
-real_path = f'../data/{DATASET_NAME}/{DATASET_NAME}_scaled.csv'
-fake_path = f'../data/{DATASET_NAME}/' + file_ending + '.csv'
-mixed_path = f'../data/{DATASET_NAME}/' + file_ending + '.csv'
+        return x, y
+
+    def split_data(self, dataset_path):
+        x, y = self.read_data(dataset_path)
+
+        self.classes = np.unique(y)
+
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=16)
+
+        return X_train, X_test, y_train, y_test
+
+    def evaluate_classifier(self, clf, dataset1, dataset2):
+        X_train, X_test, y_train, y_test = self.split_data(dataset1)
+        x_2, y_2 = self.read_data(dataset2)
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(x_2)
+
+        if len(self.classes) == 2:
+            y_pred_probs = clf.predict_proba(x_2)[:, 1]  # Predicted probabilities of the positive class
+        else:
+            y_pred_probs = clf.predict_proba(x_2)  # Predicted probabilities of all classes
+
+        # Binarize the true labels, in case it is a multiclass problem
+        y_true = label_binarize(y_2, classes=self.classes)
+
+        f1 = f1_score(y_2, y_pred, average='weighted')
+
+        return f1, y_true, y_pred_probs
+
+    def print_results(self, real_data, synth_data, mixed_data):
+
+        classifiers = {
+            "Random Forest": RandomForestClassifier(n_estimators=1000, random_state=42),
+            "Logistic Regression": LogisticRegression(C=100, penalty='l2', solver='newton-cg'),
+            "MLP Classifier": MLPClassifier(max_iter=300, activation='relu', solver='adam')
+        }
+
+        results = []
+        train_data = None
+        test_data1 = None
+        test_data2 = None
+
+        fig, axes = plt.subplots(len(classifiers), 3, figsize=(15, 5 * len(classifiers)), squeeze=False)
+
+        for idx, (clf_name, clf) in enumerate(classifiers.items()):
+            for jdx, (data_name, data) in enumerate(
+                    {"real": real_data, "synth": synth_data, "mixed": mixed_data}.items()):
+
+                ax = axes[idx, jdx]
+
+                if data_name == "real":
+                    train_data = real_data
+                    test_data1 = real_data
+                    test_data2 = synth_data
+                elif data_name == "synth":
+                    train_data = synth_data
+                    test_data1 = real_data
+                    test_data2 = synth_data
+                elif data_name == "mixed":
+                    train_data = mixed_data
+                    test_data1 = real_data
+                    test_data2 = mixed_data
+
+                if train_data is not None:
+                    f1, y_true, y_score = self.evaluate_classifier(clf, train_data, test_data1)
+                    f1_2, y_true_2, y_score_2 = self.evaluate_classifier(clf, train_data, test_data2)
+
+                    results.append({
+                        'classifier': clf_name,
+                        'train_data': data_name,
+                        'f1_real': f1,
+                        'f1_synth': f1_2
+                    })
+
+                    n_classes = len(self.classes)
+
+                    if n_classes == 2:
+                        plot_binaryclass_roc(y_true, y_score, y_true_2, y_score_2, n_classes, f'{clf_name}_{data_name}',
+                                             ax=ax)
+                    else:
+                        plot_multiclass_roc(y_true, y_score, y_true_2, y_score_2, n_classes, f'{clf_name}_{data_name}',
+                                            ax=ax, plot_class_curves=False)
+
+        plt.tight_layout()
+        plt.show()
+
+        df = pd.DataFrame(results)
+        return df
 
 
-def read_data(dataset_path):
-    dataset = pd.read_csv(dataset_path)
+# create a new instance of DataConfig, with the dataset (lower_back_pain, obesity)
+config = DataConfig(dataset_name='obesity', model_name='ctgan', epochs=800, batch_size=100)
 
-    x = dataset.iloc[:, :-1].values
-    y = dataset.iloc[:, -1].values
+real_path, fake_path, mixed_path = config.real_path, config.fake_path, config.mixed_path
 
-    return x, y
-
-
-def split_data(dataset_path):
-    x, y = read_data(dataset_path)
-
-    global classes
-    classes = np.unique(y)
-
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=16)
-
-    return X_train, X_test, y_train, y_test
-
-
-def evaluate_classifier(clf, dataset1, dataset2):
-    X_train, X_test, y_train, y_test = split_data(dataset1)
-    x_2, y_2 = read_data(dataset2)
-
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(x_2)
-
-    y_pred_probs = clf.predict_proba(x_2)[:, 1]  # Predicted probabilities of the positive class
-
-    f1 = f1_score(y_2, y_pred, average='weighted')
-
-    return f1, y_2, y_pred_probs
-
-
-def print_results(real_data, synth_data, mixed_data):
-    classifiers = {
-        "Random Forest": RandomForestClassifier(n_estimators=1000, random_state=42),
-        "Logistic Regression": LogisticRegression(C=100, penalty='l2', solver='newton-cg'),
-        "MLP Classifier": MLPClassifier(max_iter=300, activation='relu', solver='adam')
-    }
-
-    results = []
-    train_data = None
-    test_data1 = None
-    test_data2 = None
-
-    fig, axes = plt.subplots(len(classifiers), 3, figsize=(15, 5 * len(classifiers)), squeeze=False)
-
-    for idx, (clf_name, clf) in enumerate(classifiers.items()):
-        for jdx, (data_name, data) in enumerate({"real": real_data, "synth": synth_data, "mixed": mixed_data}.items()):
-
-            ax = axes[idx, jdx]
-
-            if data_name == "real":
-                train_data = real_data
-                test_data1 = real_data
-                test_data2 = synth_data
-            elif data_name == "synth":
-                train_data = synth_data
-                test_data1 = real_data
-                test_data2 = synth_data
-            elif data_name == "mixed":
-                train_data = mixed_data
-                test_data1 = real_data
-                test_data2 = mixed_data
-
-            if train_data is not None:
-                train_test1_f1, y_true, y_score = evaluate_classifier(clf, train_data, test_data1)
-                train_test2_f1, y_true_2, y_score_2 = evaluate_classifier(clf, train_data, test_data2)
-
-                results.append({
-                    'classifier': clf_name,
-                    'train_data': data_name,
-                    'f1_real': train_test1_f1,
-                    'f1_synth': train_test2_f1
-                })
-
-                plot_roc_binaryclass(y_true, y_score, y_true_2, y_score_2, f'{clf_name}_{data_name}', ax=ax)
-
-    plt.tight_layout()
-    plt.show()
-
-    df = pd.DataFrame(results)
-    return df
-
-
-print_results(real_path, fake_path, mixed_path)
-
-"""global classes
-clf = RandomForestClassifier(n_estimators=1000, random_state=42)
-_, y_true, y_score = evaluate_classifier(clf, real_path, fake_path)
-y_true = label_binarize(y_true, classes=classes)
-
-plot_multiclass_roc(y_true, y_score)"""
+evaluator = ClassifierEvaluator()
+evaluator.print_results(real_path, fake_path, mixed_path)
--- utilities/plots.py
+++ utilities/plots.py
@@ -1,24 +1,46 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import auc, roc_curve
 
 
-def plot_roc_binaryclass(y_true_real, y_score_real, y_true_synth, y_score_synth, clf_name, ax=None):
+def compute_roc_auc(y_true, y_score, n_classes):
+    # Multiclass problem
+    if n_classes > 2:
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+        mean_tpr /= n_classes
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Binary class problem
+    else:
+        fpr, tpr, _ = roc_curve(y_true, y_score)
+        roc_auc = auc(fpr, tpr)
+
+    return fpr, tpr, roc_auc
+
+
+def plot_binaryclass_roc(y_true_real, y_score_real, y_true_synth, y_score_synth, n_classes, clf_name, ax=None):
     """Args:
         y_true_real: True labels of the binary classifier for real data.
         y_score_real: Predicted scores or probabilities of the positive class for real data.
         y_true_synth: True labels of the binary classifier for synthetic data.
         y_score_synth: Predicted scores or probabilities of the positive class for synthetic data.
-        classifier_name: Name of the classifier for setting the title of the plot."""
+        clf_name: Name of the classifier for setting the title of the plot.
+        n_classes: number of classes for the classification problem"""
 
-    # Compute the false positive rate, true positive rate, and thresholds for real data
-    fpr_real, tpr_real, thresholds_real = roc_curve(y_true_real, y_score_real)
-    # Compute the false positive rate, true positive rate, and thresholds for synthetic data
-    fpr_synth, tpr_synth, thresholds_synth = roc_curve(y_true_synth, y_score_synth)
-    # Compute the area under the ROC curve for real data
-    roc_auc_real = auc(fpr_real, tpr_real)
-    # Compute the area under the ROC curve for synthetic data
-    roc_auc_synth = auc(fpr_synth, tpr_synth)
+    fpr_real, tpr_real, roc_auc_real = compute_roc_auc(y_true_real, y_score_real, n_classes)
+    fpr_synth, tpr_synth, roc_auc_synth = compute_roc_auc(y_true_synth, y_score_synth, n_classes)
 
     ax.plot(fpr_real, tpr_real, color='b', label='Real (AUC = %0.2f)' % roc_auc_real)
     ax.plot(fpr_synth, tpr_synth, color='g', label='Synthetic (AUC = %0.2f)' % roc_auc_synth)
@@ -29,56 +51,29 @@ def plot_roc_binaryclass(y_true_real, y_score_real, y_true_synth, y_score_synth,
     ax.legend()
 
 
-def plot_multiclass_roc(y_true_1, y_score_1, n_classes, plot_class_curves=True):
-    # Compute ROC curve and ROC area for each class
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_true_1[:, i], y_score_1[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_true_1.ravel(), y_score_1.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-    # Compute macro-average ROC curve and ROC area
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
-    mean_tpr /= n_classes
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-    # Plot ROC curve
-    plt.figure(figsize=(8, 6))
+def plot_multiclass_roc(y_true_real, y_score_real, y_true_synth, y_score_synth, n_classes, clf_name,
+                        plot_class_curves=True, ax=None):
+    fpr_real, tpr_real, roc_auc_real = compute_roc_auc(y_true_real, y_score_real, n_classes)
+    fpr_synth, tpr_synth, roc_auc_synth = compute_roc_auc(y_true_synth, y_score_synth, n_classes)
     lw = 2
-    plt.plot(fpr["micro"], tpr["micro"], label='micro-average ROC curve (AUC = {0:0.2f})'
-                                               ''.format(roc_auc["micro"]),
-             color='deeppink', linestyle=':', linewidth=4)
-
-    plt.plot(fpr["macro"], tpr["macro"],
-             label='macro-average ROC curve (AUC = {0:0.2f})'
-                   ''.format(roc_auc["macro"]),
-             color='navy', linestyle=':', linewidth=4)
-
+    ax.plot(fpr_real["micro"], tpr_real["micro"], label='micro-average - Real (AUC = {0:0.2f})'
+                                                        ''.format(roc_auc_real["micro"]),
+            color='deeppink', linestyle=':', linewidth=4)
+    ax.plot(fpr_real["macro"], tpr_real["macro"],
+            label='macro-average - Real (AUC = {0:0.2f})'.format(roc_auc_real["macro"]),
+            color='navy', linestyle=':', linewidth=4)
+    ax.plot(fpr_synth["macro"], tpr_synth["macro"],
+            label='macro-average - Synthetic (AUC = {0:0.2f})'.format(roc_auc_synth["macro"]),
+            color='darkorange', linestyle=':', linewidth=4)
     if plot_class_curves:
-        colors = plt.cm.get_cmap('tab10', n_classes)(range(n_classes)).tolist()
-        for i, color in zip(range(n_classes), colors):
-            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
-                     label='ROC curve of class {0} (AUC = {1:0.2f})'
-                           ''.format(i, roc_auc[i]))
+        colors_real = ax.cm.get_cmap('tab10', n_classes)(range(n_classes)).tolist()
+        colors_synth = ax.cm.get_cmap('tab10', n_classes)(range(n_classes)).tolist()
+        [ax.plot(fpr_real[i], tpr_real[i], color=colors_real[i], lw=lw) for i in range(n_classes)]
+        [ax.plot(fpr_synth[i], tpr_synth[i], color=colors_synth[i], lw=lw, linestyle='--',
+                 label='Class {0} - Synthetic (AUC = {1:0.2f})'.format(i, roc_auc_synth[i])) for i in range(n_classes)]
 
-    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    if plot_class_curves:
-        plt.legend(loc="lower right")
-    else:
-        plt.legend(loc="best")
-    plt.show()
+    ax.plot([0, 1], [0, 1], 'k--', lw=lw)
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('ROC Curve for {}'.format(clf_name))
+    ax.legend(loc="lower right")
